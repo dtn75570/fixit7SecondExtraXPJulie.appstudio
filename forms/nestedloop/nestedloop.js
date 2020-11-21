604fd33655d4653dfb25fc5428b52d5426a88040
dtn75570 <71202757+dtn75570@users.noreply.github.com>
2020-11-20 20:26:22 -0600

    Initial commit

---
+++ forms/nestedloop/nestedloop.js
@@ -0,0 +1,9 @@
+let num = prompt("Please enter the number of rows you want.")
+let num2 = prompt("Please enter the number of columns you want.")
+
+for (let x = 0; x < num ; x++) {
+  for (let y2 = 0; y2 < num2 ; y2++) {
+    document.write(".")
+    }
+  document.writeln("<br />")
+}
